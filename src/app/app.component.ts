ce5ff7d9a40755c16369c1c5fbde371974e4a9ab
pk <pappu.kumar@fluper.in>
2024-09-10 15:35:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'promises';
+
+    constructor(private http: HttpClient) {
+
+    }
+    ngOnInit() {
+        const promise  = new Promise<string>((resolve, reject) => {
+            setTimeout(() => {
+                resolve("NOw resolved promise");
+            }, 1000);
+        })
+
+        promise.then((success) => { 
+            console.log(success); 
+        })
+        .catch((error) => { 
+            console.log(error); 
+        }); 
+        // Output: Promise Resolved
+        console.log(promise);
+     
+        this.getData();
+        // this.ObeservableExample()
+    }
+    getData(): Promise<any> {
+        // Create and return a new promise
+        return new Promise((resolve, reject) => {
+            // Use HttpClient to make a GET request to an API endpoint
+            const promise = this.http.get('https://jsonplaceholder.typicode.com/posts')            
+            .subscribe({
+                next: (v) => {
+                    console.log(v)
+                    resolve(promise);
+                },
+                error: (e) => {
+                    console.error(e)
+                    reject(e);  
+                },
+                complete: () => console.info('complete')
+            })
+           
+      
+            
+        });        
+    }
+
+    ObeservableExample(){
+        const observable = this.http.get('https://jsonplaceholder.typicode.com/posts');
+
+        // Subscribe to the observable and log the response data to the console
+        observable.subscribe(
+            {
+                next: (v) => console.log(v),
+                error: (e) => console.error(e),
+                complete: () => console.info('complete') 
+            }
+        );
+    }
 }
